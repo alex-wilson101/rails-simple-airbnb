08adaf0da4352484c26e73b1a730840ebd159d5a
Alex Wilson <alexnjwilson@gmail.com>
2023-08-23 15:31:01 +0100

    asdf

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Flat.create!(
+  name: 'cute family home London',
+  address: '5 hebron road London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 115,
+  number_of_guests: 2
+)
+Flat.create!(
+  name: 'Grand Terrice house London',
+  address: '87 Teighnmouth road London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 8
+)
+Flat.create!(
+  name: 'Bright and airy house London',
+  address: '40 upcerne road London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 85,
+  number_of_guests: 6
+)
+Flat.create!(
+  name: 'modern muse house London',
+  address: '24 artisan muse London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 140,
+  number_of_guests: 3
+)
